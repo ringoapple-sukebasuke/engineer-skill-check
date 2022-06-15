539e94174e8115c9ea5c1e7c045d66e7191e62ac
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-06-15 20:10:16 +0900

    お知らせ登録createアクションの作成。ストロングパラメーター使用

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,25 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.order(created_at: :desc)
+  end
 
   def new
     @article = Article.new
   end
   
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render new_article_path
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
