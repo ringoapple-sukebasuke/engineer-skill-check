9df48f80fcae7b826f8148757087ae2a54065920
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-06-16 14:05:33 +0900

    お知らせ機能destroy完成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,6 +31,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: "「#{@article.title}」を削除しました。"
+  end
+
   private
 
   def article_params
