4110c9545241beac3b38c02e3081d7ef3f2639e1
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-06-16 17:32:11 +0900

    ログインユーザーと投稿ユーザーを紐付け、自分が投稿したお知らせのみ編集削除可能に

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content).merge(Author: current_user.id)
   end
 
   def sort_column
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true
+  validates :Author, presence: true
   scope :active, -> {
     where(deleted_at: nil)
   }
