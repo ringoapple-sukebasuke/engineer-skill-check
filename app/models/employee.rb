41f207540e246a8ae7933972bfdd0bdb4ac68582
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-06-16 18:28:28 +0900

    dependent: :destroyの追加

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
   belongs_to :office
   belongs_to :department
-  has_many :profiles
+  has_many :profiles, dependent: :destroy
 
   validates :number, presence: true, uniqueness: true
   validates :last_name, presence: true
